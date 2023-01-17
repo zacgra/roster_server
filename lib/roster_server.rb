c19bcd9e4d197944d7b7c073eb3e7c9c5cd4f006
Zach Grammon <zachgrammon@gmail.com>
2023-01-17 19:23:44 +0000

    Initialize gem

---
+++ lib/roster_server.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "roster_server/version"
+
+module RosterServer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/roster_server_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RosterServer do
+  it "has a version number" do
+    expect(RosterServer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
