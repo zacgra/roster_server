0e59846b82519912e048d1e71181548224e9ae49
Zach Grammon <zachgrammon@gmail.com>
2023-01-18 05:26:48 +0000

    Add Client class to create connection

---
+++ lib/roster_server/client.rb
@@ -0,0 +1,31 @@
+require 'faraday'
+require 'faraday_middleware'
+require 'oauth'
+require 'simple_oauth'
+
+module RosterServer
+  class Client
+    attr_reader :base_url, :client_id, :client_secret, :adapter
+
+    def initialize(base_url:, client_id:, client_secret:, adapter: Faraday.default_adapter)
+        @base_url = base_url
+        @client_id = client_id
+        @client_secret = client_secret
+        @adapter = adapter
+    end
+
+    def connection
+      @connection ||= Faraday.new do |conn|
+        conn.url_prefix = @base_url
+        conn.request :oauth, consumer_key: @client_id, consumer_secret: @client_secret
+        conn.request :json
+        conn.response :json, content_type: "application/json"
+        conn.adapter adapter
+      end
+    end
+
+    def inspect
+      "#<RosterServer::Client"
+    end
+  end
+end
